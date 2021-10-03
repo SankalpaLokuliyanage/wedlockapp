fc0c122c7b13cb64fd14db2d51393cc479214c5b
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-03 16:07:13 +0530

    add animation to splash activity

--- app/src/main/java/com/example/wedlock/SplashActivity.java
+++ app/src/main/java/com/example/wedlock/SplashActivity.java
@@ -2,13 +2,38 @@ package com.example.wedlock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 4000;
+
+    ImageView logo;
+    Animation sideAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logo = findViewById(R.id.logo);
+
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+
+        logo.setAnimation(sideAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, SPLASH_TIMER);
+
+
     }
 }
