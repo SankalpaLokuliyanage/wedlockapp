c3180effc5c2ed7ddba5defac4611ba49931b1c2
SankalpaLokuliyanage <c.s.lokuliyanage@wlv.ac.uk>
2021-10-05 22:47:53 +0530

    company signup screen

--- app/src/main/java/com/example/wedlock/SignupAdvertiser.java
+++ app/src/main/java/com/example/wedlock/SignupAdvertiser.java
@@ -3,6 +3,11 @@ package com.example.wedlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SignupAdvertiser extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class SignupAdvertiser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_advertiser);
+
+        List<String> service = Arrays.asList("Hotel", "Decors", "Cars", "Salon", "Wedding Cakes", "DJ", "Dance Group");
+
+        Spinner spinner = findViewById(R.id.selectedService);
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.signup_selected_item, service);
+        adapter.setDropDownViewResource(R.layout.signup_dropdown_item);
+
+        spinner.setAdapter(adapter);
     }
 }
